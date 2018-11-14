b2165334a5c6ce524829e392e86813d0cb209feb
Hannah <hannahstewart23@yahoo.co.uk>
2018-11-14 16:29:41 +0000

    added test for api key in logic file

--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,14 @@
 // all logic stuff to go here
+// function sum(a, b) {
+//   return a + b;
+// }
+var weather = {
+responseApi: function(code){
+  var code = 200;
+  return code;
+}
+};
+
+if (typeof module!== "undefined") {
+  module.exports = weather;
+};
--- tests/weather.test.js
+++ tests/weather.test.js
@@ -1,5 +1,17 @@
-const sum = require('./sum');
+const weather = require('../js/logic');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test('tests if user inputs a string', () => {
+  expect(str(input.valid)).toBe(true);
+});
+
+test('tests if we get a response from the api', () => {
+  expect(weather.responseApi()).toBe(200);
+});
+
+test('tests if the is a response from music API', () => {
+  expect(weather.responseApi()).toBe(200);
 });
