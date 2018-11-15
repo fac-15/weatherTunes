bd3f92e43cb949856974c8493deb0e5a4ea65d0a
Hannah <hannahstewart23@yahoo.co.uk>
2018-11-15 17:18:10 +0000

    added get music

--- js/logic.js
+++ js/logic.js
@@ -17,8 +17,30 @@ const weatherFunctions = {
 
     xhr.open("GET", url, true);
     xhr.send();
+  },
+
+  getMusic: function(weatherResults) {
+
+    console.log("getMusic", weatherResults);
+
+    const xhr = new XMLHttpRequest();
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&order=rating&q=${weatherResults + " song lyrics music"}&type=video&videoCaption=closedCaption&videoDefinition=high&videoEmbeddable=true&key=${keys.youtube}`
+    ;
+
+    xhr.onreadystatechange = function() {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+    const music = (JSON.parse(xhr.responseText));
+    const videoId = music.items[0].id.videoId;
+    document.getElementById("video").src = `https://www.youtube.com/embed/${videoId}`
+     }
+    };
+
+     xhr.open("GET", url, true);
+     xhr.send();
   }
 };
+
+
 
 // const newTodo = todoFunctions.todoFunctions();
 
